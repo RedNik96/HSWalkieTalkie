0a89de901e72f36ec30ef716b4fefee8615a2bc3
RedNik96 <nikidev96@yahoo.de>
2016-04-26 12:27:11 +0200

    Anpassung der Pfades an die neue Struktur

--- src/templates/layout.php
+++ src/templates/layout.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Musikverwaltung</title>
-    <link rel="stylesheet" href="standard.css">
+    <link rel="stylesheet" href="../public/css/standard.css">
 </head>
 <body>
     <?= $content_for_layout ?>
