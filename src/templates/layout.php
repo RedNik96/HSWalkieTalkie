5fd9205a9ac6cd96e7fa9e6f4d6127eeba199fa4
keulinho <jonas.elfering@web.de>
2016-05-09 17:08:19 +0200

    settings und bootstrap version gefixt

--- src/templates/layout.php
+++ src/templates/layout.php
@@ -6,11 +6,14 @@
     <!-- Font Awesome-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
     <!-- Das neueste kompilierte und minimierte CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <!-- Optionales Theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <!-- Das neueste kompilierte und minimierte JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
 </head>
 <body>
     <div class="container-fluid full">
--- src/templates/settings.php
+++ src/templates/settings.php
@@ -1,11 +1,23 @@
 
-    <ul class="nav nav-tabs">
-        <li role="presentation" class="active"><a id='pInformation' href="#pInformation" aria-controls="pInformation" role="tab" data-toggle="tab">Persönliche Informationen</a></li>
-        <li role="presentation"><a id='konto' href="#konto" aria-controls="konto" role="tab" data-toggle="tab" >Konto</a></li>
-        <li role="presentation"><a id='ilias' href="#ilias" aria-controls="ilias" role="tab" data-toggle="tab">Ilias-Feed</a></li>
-    </ul>
-    <div class="tab-content">
-        <div role="tabpanel" class="tab-pane active" id="pInformation">persönliche Informationen</div>
-        <div role="tabpanel" class="tab-pane" id="konto">Konto</div>
-        <div role="tabpanel" class="tab-pane" id="ilias">Ilias</div>
+    <div class="container">
+        <ul class="nav nav-tabs">
+            <li class="active"><a data-toggle="tab" href="#personal">persönliche Informationen</a></li>
+            <li><a data-toggle="tab" href="#konto">Konto</a></li>
+            <li><a data-toggle="tab" href="#ilias">Ilias-Feed</a></li>
+        </ul>
+
+        <div class="tab-content">
+            <div id="personal" class="tab-pane fade in active">
+                <h3>HOME</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div id="konto" class="tab-pane fade">
+                <h3>Menu 1</h3>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <div id="ilias" class="tab-pane fade">
+                <h3>Menu 2</h3>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+            </div>
+        </div>
     </div>
