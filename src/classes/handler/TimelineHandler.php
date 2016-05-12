e4d41ce1ac3b3e8884c08119bc70b99aed4b5242
XGWSDFE <david.feldhoff@web.de>
2016-05-12 11:26:27 +0200

    Anpassung der Timeline. Vorher wurde immer nur ein Post pro Nutzer angezeigt.

--- src/classes/handler/TimelineHandler.php
+++ src/classes/handler/TimelineHandler.php
@@ -3,7 +3,7 @@
 global $dbh;
 
 $stmt = $dbh->prepare("
-  SELECT U.firstName, U.lastName, U.username, P.content, 
+  SELECT P.id AS PostID, U.firstName, U.lastName, U.username, P.content, 
     ((SELECT COUNT(V.voter) FROM votes AS V WHERE V.post = P.id AND V.vote = true) -
       (SELECT COUNT(V.voter) FROM Votes AS V WHERE V.post = P.id AND V.vote = false)) AS Votes
   FROM posts AS P, user AS U
@@ -14,7 +14,7 @@ $result = $stmt->execute(array("userid" => $_SESSION['user']));
 
 $posts = array();
 while($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $posts[$result['username']] = array(
+    $posts[$result['PostID']] = array(
         'username' => $result['username'],
         'firstName' => $result['firstName'],
         'lastName' => $result['lastName'],
