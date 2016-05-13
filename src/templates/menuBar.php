4013fecf21f8dec499b5542cabbe07a58de8f1f9
xgadles <leonstapper@gmx.de>
2016-05-13 12:07:25 +0200

    User

--- src/templates/menuBar.php
+++ src/templates/menuBar.php
@@ -17,12 +17,14 @@
         <li <? if ($GLOBALS['match']['name']=='timeline') { ?>class="active"<? } ?> > <a href="<? global $router; echo $router->generate("timeline");?>"><span class="fa fa-newspaper-o fa-2x"></span> Neuigkeiten</a></span></li>
         <li <? if ($GLOBALS['match']['name']=='profile') { ?>class="active"<? } ?> > <a href="<? global $router; echo $router->generate("profile");?>"><span class="fa fa-user fa-2x"></span> Profil</a></span></li>
         <li <? if ($GLOBALS['match']['name']=='settings') { ?>class="active"<? } ?> > <a href="<? global $router; echo $router->generate("settings");?>"><span class="fa fa-gear fa-2x"></span> Einstellungen</a></span></li>
-        <form class="navbar-form navbar-left" role="search">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Suchen">
-          </div>
-          <button type="submit" class="btn btn-default"><i class="fa fa-search" aria-hidden="true"></i> Suchen</button>
-        </form>
+        <li id="searchBar">
+          <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Suchen">
+            </div>
+            <button type="submit" class="btn btn-default"><i class="fa fa-search" aria-hidden="true"></i> Suchen</button>
+          </form>
+        </li>
       </ul>
         <ul class="nav navbar-nav navbar-right">
           <li><a href="<?= $GLOBALS['router']->generate('logoutGet'); ?>"><span class="fa fa-power-off fa-2x"></span></a></li>
