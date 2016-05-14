1274b7775ce2404179be23ee7127aa1e611dd828
xgadles <leonstapper@gmx.de>
2016-05-14 11:43:26 +0200

    Statistik Userbild einbinden

--- src/classes/handler/StatisticHandler.php
+++ src/classes/handler/StatisticHandler.php
@@ -6,7 +6,7 @@ class StatisticHandler {
         global $dbh;
 
         $stmtRichestUsers = $dbh->prepare("
-              SELECT U.firstName, U.lastName, U.username
+              SELECT U.picture , U.firstName, U.lastName, U.username
               FROM user AS U
               LIMIT 3
               ");
@@ -16,6 +16,7 @@ class StatisticHandler {
         $richestUsers = array();
         while($result = $stmtRichestUsers->fetch(PDO::FETCH_ASSOC)) {
             $richestUsers[$result['username']] = array(
+                'profilePicture' => $result['picture'],
                 'username' => $result['username'],
                 'firstName' => $result['firstName'],
                 'lastName' => $result['lastName']
