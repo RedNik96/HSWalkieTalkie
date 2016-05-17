4eb211ba33439f99cb35e4fe741a22952cfc2e81
keulinho <jonas.elfering@web.de>
2016-05-17 23:25:55 +0200

    Timeline klickbar

--- src/classes/handler/TimelineHandler.php
+++ src/classes/handler/TimelineHandler.php
@@ -46,11 +46,6 @@ Class TimelineHandler {
             $imgs[$imgCounter] = $img['filename'];
             $imgCounter = $imgCounter + 1;
         }
-        if (is_null($result['picture'])) {
-            $result['picture'] = "/HSWalkieTalkie/src/img/profile_default.png";
-        } else {
-            $result['picture'] = "/HSWalkieTalkie/src/img/profile/" . $result['picture'];
-        }
         $posts[$result['postID']] = array(
             'postID'    => $result['postID'],
             'username'  => $result['username'],
--- src/templates/timeline.php
+++ src/templates/timeline.php
@@ -25,12 +25,7 @@
         <!--<form class="post">-->
             <div class="postheader">
                 <div class="postauthor">
-
-                    <img class="img-rounded" src="<?= $post['picture']; ?>" alt="Bild">
-                    <div class="postauthorname">
-                        <span class="name"><?= htmlspecialchars($post['firstName']) . " " . htmlspecialchars($post['lastName'])?></span>
-                        <span class="username">@<?= htmlspecialchars($post['username']); ?></span>
-                    </div>
+                    <? echo User::getUserHtml($post['picture'], $post['firstName'], $post['lastName'], $post['username'])?>
                 </div>
                 <div class="posttime">
                     <span class="time"><?= htmlspecialchars($post['datePosted']); ?> Uhr</span>
