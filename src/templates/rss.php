2f918426001485efe9784c18c7fa4d51d3c8d608
Marius <m.mamsch@gmx.de>
2016-05-18 19:51:09 +0200

    rss url

--- src/templates/rss.php
+++ src/templates/rss.php
@@ -1,3 +1,8 @@
+
 <span class="rssFeed">
-    <a>Hier könnte der HSW-Feed stehen</a>
+    <legend>ILIAS-RSS-Feed</legend>
 </span>
+<?php
+RSSHandler::getRssfeed("https://www.hsw-elearning.de/feed.php?client_id=baw_06&user_id=63201&hash=fa86176980018ac0b3d37c85af61e468", "rssFeed", "auto", 5, 0);
+//https://www.hsw-elearning.de/feed.php?client_id=baw_06&user_id=63201&hash=fa86176980018ac0b3d37c85af61e468
+//                  $rssfeed,  $cssclass="../public/css/rssFeed.css", $encode="auto", $anzahl=10, $mode=0
