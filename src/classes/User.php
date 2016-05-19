258c7eafd61847417e37c6c55dcf7e23ead4540c
xgadles <leonstapper@gmx.de>
2016-05-19 18:35:51 +0200

    Kommentare und Farbe für die Suche

--- src/classes/User.php
+++ src/classes/User.php
@@ -2,6 +2,14 @@
 
 class User {
 
+    /**
+     * Diese Funktion gibt für die übergebenen Nutzerdaten ein div-Template zurück, in dem die Userdaten aufbereitet dargestellt werden.
+     * @param $profilePicLink Link zum Profilbild des Nutzers
+     * @param $firstname Vorname des Nutzers
+     * @param $lastname Nachname des Nutzers
+     * @param $username Benutzername des Nutzers
+     * @return string Aufbereitete Userdaten als HTML-Text
+     */
     public static function getUserHtml($profilePicLink, $firstname, $lastname, $username){
         global $router;
         $userhtml = "<link rel=\"stylesheet\" href=\"/HSWalkieTalkie/src//public/css/user.css\">
