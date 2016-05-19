34cb39cfac0dc870a02ed845d087a90d493b0d10
Marius <m.mamsch@gmx.de>
2016-05-19 14:36:36 +0200

    rss feed eingebunden

--- src/templates/rss.php
+++ src/templates/rss.php
@@ -1,11 +1,25 @@
 
+<link rel="stylesheet" type="text/css" href="/HSWalkieTalkie/src/public/css/rssFeed.css">
 <span class="rssFeed">
-
     <legend>ILIAS-RSS-Feed</legend>
 </span>
-<?php
-RSSHandler::getRssfeed();
+<?foreach ($rss_article as $entry){?>
+    <div class="rss_entry">
+        <p>
+            <div class="rss_title">
+                <? echo $entry['title']?>
+            </div>
+            <div class="rss_date">
+                <span class="rss_date_pre">
+                    <? echo 'Datum: '?>
+                </span>
+                <?echo $entry['pubDate']?>
+            </div>
+            <div class="rss_link">
+                <a href="<?=$entry['link']?>">zur ILIAS-Datei</a>
+            </div>
+        </p>
+    </div>
+<?}?>
 
-?>
-</span>
 
