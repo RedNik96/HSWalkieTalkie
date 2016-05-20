3e8f4f09b01de0fd4d4ae29cd9901a8a69626515
keulinho <jonas.elfering@web.de>
2016-05-20 13:16:11 +0200

    error handler mit message

--- src/classes/handler/ErrorHandler.php
+++ src/classes/handler/ErrorHandler.php
@@ -8,13 +8,13 @@ class ErrorHandler
     /**
      * rendert die Fehlerseite ohne die Menubar und die Anzeigen rechts und links
      */
-    public static function showError() {
+    public static function showError($error="") {
         $templates=array(
             'template_right' => null,
             'template_left' => null,
             'template_top' => null
         );
-        $data=array();
+        $data=array('error' => $error);
         Template::render('error',$data,$templates);
     }
 }
